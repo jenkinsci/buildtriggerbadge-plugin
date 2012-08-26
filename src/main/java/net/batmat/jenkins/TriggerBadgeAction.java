e07515980956910cad5092c8a4e2e040ada62da7
Baptiste Mathus <batmat@batmat.net>
2012-08-26 12:58:19 +0200

    Added code and (ugly) images for having different icon for each cause.
    Notice: the images are VERY ugly. This set was done is less than 5
    minutes just to show a different image is shown :-).
    I think I'll try to create better icons later, or even better
    someone with good design abilities will do it ;-).

--- src/main/java/net/batmat/jenkins/TriggerBadgeAction.java
+++ src/main/java/net/batmat/jenkins/TriggerBadgeAction.java
@@ -2,6 +2,10 @@ package net.batmat.jenkins;
 
 import hudson.model.BuildBadgeAction;
 import hudson.model.Cause;
+import hudson.model.Cause.UpstreamCause;
+import hudson.model.Cause.UserCause;
+import hudson.triggers.SCMTrigger.SCMTriggerCause;
+import hudson.triggers.TimerTrigger.TimerTriggerCause;
 
 public class TriggerBadgeAction implements BuildBadgeAction {
 
@@ -26,7 +30,23 @@ public class TriggerBadgeAction implements BuildBadgeAction {
 	}
 
 	public String getIcon() {
-		return "/plugin/triggerbadge/images/trigger-badge.png";
+		if(SCMTriggerCause.class.isInstance(buildCause))
+		{
+			return IconScmCause;
+		}
+		else if(UserCause.class.isInstance(buildCause))
+		{
+			return IconUserCause;
+		}
+		else if(UpstreamCause.class.isInstance(buildCause))
+		{
+			return IconUpstreamCause;
+		}
+		else if(TimerTriggerCause.class.isInstance(buildCause))
+		{
+			return IconTimerCause;
+		}
+		return IconCatchAllCause;
 	}
 
 	public String getUrlName() {
@@ -36,4 +56,11 @@ public class TriggerBadgeAction implements BuildBadgeAction {
 	public String getIconFileName() {
 		return null;
 	}
+	
+	private static final String IconUpstreamCause = "/plugin/triggerbadge/images/trigger-badge-upstream-cause.png";
+	private static final String IconScmCause = "/plugin/triggerbadge/images/trigger-badge-scm-cause.png";
+	private static final String IconUserCause = "/plugin/triggerbadge/images/trigger-badge-user-cause.png";
+	private static final String IconTimerCause = "/plugin/triggerbadge/images/trigger-badge-timer-cause.png";
+	private static final String IconCatchAllCause = "/plugin/triggerbadge/images/trigger-badge.png";
+	
 }
