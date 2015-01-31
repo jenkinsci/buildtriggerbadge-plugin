87564277341fb3f9ade727864cb1776e57364b9b
Baptiste Mathus <batmat@batmat.net>
2015-01-31 07:07:22 +0100

    Harnessing the causes support a bit

--- src/test/java/org/jenkinsci/plugins/buildtriggerbadge/RunListenerImplTest.java
+++ src/test/java/org/jenkinsci/plugins/buildtriggerbadge/RunListenerImplTest.java
@@ -1,11 +1,15 @@
 package org.jenkinsci.plugins.buildtriggerbadge;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import hudson.model.BuildBadgeAction;
 import hudson.model.FreeStyleBuild;
+import hudson.model.Cause;
+import hudson.model.Cause.RemoteCause;
 import hudson.model.FreeStyleProject;
 import hudson.triggers.SCMTrigger.SCMTriggerCause;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -35,5 +39,23 @@ public class RunListenerImplTest {
 		FreeStyleBuild build = futureBuild.get();
 		// there should be only one badge action here
 		assertThat(build.getBadgeActions()).hasSize(1);
+	}
+
+	@Test
+	public void checkBadges() throws Exception {
+		checkBuildCause(new SCMTriggerCause("bim"), "scm-cause.png");
+		checkBuildCause(new RemoteCause("hop", "kk"), "remote-cause.png");
+	}
+
+	private void checkBuildCause(Cause buildCause, String expected) throws IOException, InterruptedException, ExecutionException {
+		FreeStyleProject project = j.createFreeStyleProject();
+		Future<FreeStyleBuild> futureBuild = project.scheduleBuild2(0, buildCause);
+		FreeStyleBuild build = futureBuild.get();
+		List<BuildBadgeAction> badgeActions = build.getBadgeActions();
+		assertThat(badgeActions).hasSize(1);
+		BuildBadgeAction badgeAction = badgeActions.get(0);
+		assertThat(badgeAction).isInstanceOf(BuildTriggerBadgeAction.class);
+		BuildTriggerBadgeAction buildTriggerBadgeAction = (BuildTriggerBadgeAction) badgeAction;
+		assertThat(buildTriggerBadgeAction.getIcon()).contains(expected);
 	}
 }
